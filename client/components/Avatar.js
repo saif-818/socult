89bc4cecda8329fe45a01b32d0a33c6f1a035b2c
Saifuddin <85546997+saif-818@users.noreply.github.com>
2023-03-10 08:41:05 +0530

    profile image of user

---
+++ client/components/Avatar.js
@@ -0,0 +1,7 @@
+export default function Avatar() {
+    return(
+        <div className={`w-12 h-12 rounded-full overflow-hidden`}>
+            <img src="https://images.unsplash.com/photo-1591084728795-1149f32d9866?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"></img>
+        </div>
+    );
+}
